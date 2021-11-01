7e1da5598762df6b807fce7e7fc1a01aa6ae0ce7
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2021-11-01 19:33:08 +0700

    Tampilan login dan register

---
+++ resources/views/authentication/login.blade.php
@@ -0,0 +1,63 @@
+@extends('authentication.layouts.main')
+
+@section('page-content')
+<div class="col-lg-5 col-md-7">
+    <div class="card bg-secondary border-0 mb-0">
+        <div class="card-header bg-transparent">
+            <div class="text-muted text-center mt-2 mb-2"><h1>{{ $title }}</h1></div>
+        </div>
+        <div class="card-body px-lg-5 py-lg-5">
+            <form action="/login" method="post" role="form">
+                @csrf
+                <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-single-02"></i></span>
+                        </div>
+                        <input class="form-control" name="username" placeholder="Username" type="text" required>
+                    </div>
+                    @error('username')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                        </div>
+                        <input class="form-control" name="password" placeholder="Password" type="password" required>
+                    </div>
+                    @error('password')
+                    <div class="alert alert-danger">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <div class="row my-4">
+                    <div class="col-12">
+                    <div class="custom-control custom-control-alternative custom-checkbox">
+                        <input class="custom-control-input" id="customCheckRegister" type="checkbox">
+                        <label class="custom-control-label" for="customCheckRegister">
+                        <span class="text-muted">I agree with the <a href="#!">Privacy Policy</a></span>
+                        </label>
+                    </div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary mt-4">Masuk</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-6">
+        <a href="#" class="text-light"><small>Forgot password?</small></a>
+        </div>
+        <div class="col-6 text-right">
+        <a href="/register" class="text-light"><small>Registrasi akun</small></a>
+        </div>
+    </div>
+</div>
+@endsection
